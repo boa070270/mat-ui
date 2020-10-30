6adfe2721bf631f1b1040c9fa3bace397ece50b5
Oleh Bilous <boa070270@gmail.com>
2020-10-30 19:49:43 +0200

    continue ...

--- projects/ui-lib-showcase/src/app/carousel-test/carousel-test.component.ts
+++ projects/ui-lib-showcase/src/app/carousel-test/carousel-test.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {ObservableWithRefresh, UIDataSource} from 'ui-lib';
+import {from, generate, Observable} from 'rxjs';
+import {fromArray} from 'rxjs/internal/observable/fromArray';
+import {map} from 'rxjs/operators';
+import {fromFetch} from 'rxjs/fetch';
+import {fromPromise} from 'rxjs/internal-compatibility';
+import {TestData} from '../common-api.service';
 
 @Component({
   selector: 'app-carousel-test',
@@ -6,10 +13,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel-test.component.sass']
 })
 export class CarouselTestComponent implements OnInit {
+  datasource: UIDataSource<any> = new TextDataSource();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+}
+interface Data {
+  src: string;
+}
+
+const FILES = [
+  'alice.jpg', 'b1.PNG', 'background.jpg', 'barmalesha.jpg', 'black.jpg', 'fiona.jpg', 'i2.PNG', 'logo.jpg', 'monya.jpg', 'nick.jpg', 'nyura.jpg', 'osya.jpg', 'rubik.jpg', 'tasya.jpg', 'taya.jpg', 'tihon.jpg', 'una.jpg'
+];
+
+class TextDataSource implements UIDataSource<Data> {
+  observable: ObservableWithRefresh<Data>;
+  pos = 0;
+
+  constructor() {
+    this.observable = new ObservableWithRefresh<Data>(this.newGenerator());
+  }
+
+  newGenerator(): Observable<Data[]> {
+    if (this.pos === (FILES.length - 1)) {
+      this.pos = 0;
+    }
+    let end = this.pos + 7;
+    if (end >= FILES.length) {
+      end = FILES.length - 1;
+    }
+    const next = FILES.slice(this.pos, end);
+    this.pos = end;
+    return fromPromise(Promise.resolve(next.map(value => ({src: '/assets/carousel/' + value}))));
+  }
+
+  delete(rows: Data[]): Observable<any> {
+    return undefined;
+  }
+
+  insert(row: Data): Observable<any> {
+    return undefined;
+  }
+
+  refresh(): void {
+    this.observable.newSource(this.newGenerator());
+  }
+
+  select(filter?: any): Observable<Data[]> {
+    return this.observable;
+  }
+
+  update(row: Data): Observable<any> {
+    return undefined;
   }
 
 }
--- projects/ui-lib/src/lib/carousel.component.ts
+++ projects/ui-lib/src/lib/carousel.component.ts
@@ -1,16 +1,83 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, OnDestroy, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {ComponentType} from '@angular/cdk/overlay';
+import {DataSource} from '@angular/cdk/collections';
+import {UIDataSource} from './ui-types';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'lib-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent<T> implements OnInit, OnDestroy {
   @ViewChild('carousel') container: ElementRef<HTMLDivElement>;
+  @Input() itemTemplate: ComponentType<T> | TemplateRef<T>;
+  @Input() datasource: UIDataSource<T>;
+  @Input() showNumberItems: number;
+  @Input() heightCarousel: number;
+  @Input() widthItem: number;
+  items: T[] = [];
+  allData: T[] = [];
+  subscription: Subscription;
+  intervalID;
+  position = 0;
+  cycles = 0;
 
-  constructor() { }
-
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ]).subscribe(result => {
+        if (result.matches) {
+          if (result.breakpoints[Breakpoints.XSmall]){
+            console.log(Breakpoints.XSmall);
+          } else if (result.breakpoints[Breakpoints.Small]){
+            console.log(Breakpoints.Small);
+          } else if (result.breakpoints[Breakpoints.Medium]){
+            console.log(Breakpoints.Medium);
+          } else if (result.breakpoints[Breakpoints.Large]){
+            console.log(Breakpoints.Large);
+          } else if (result.breakpoints[Breakpoints.XLarge]){
+            console.log(Breakpoints.XLarge);
+          }
+        }
+      });
+  }
+  nextPortion(): void {
+    this.items.shift();
+    if (this.position >= this.allData.length) {
+      this.position = 0;
+      this.cycles++;
+    }
+    this.items.push(this.allData[this.position++]);
+  }
   ngOnInit(): void {
+    this.subscription = this.datasource.select().subscribe(value => {
+      if (this.showNumberItems > value.length) {
+        this.showNumberItems = value.length;
+      }
+      this.position = this.showNumberItems;
+      this.allData = value;
+      this.items = this.allData.slice(0, this.showNumberItems);
+    });
+    this.intervalID = setInterval(() => {
+        this.nextPortion();
+        if (this.cycles > 5) {
+          this.datasource.refresh();
+        }
+    }, 5000);
+  }
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    if (this.intervalID) {
+      clearInterval(this.intervalID);
+    }
   }
 
 }
